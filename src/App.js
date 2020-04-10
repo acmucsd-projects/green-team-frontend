b0a43b4936dfa515e98a848f61202120576e624e
YiyueMaggieMao <myykitty711@gmail.com>
2020-04-10 08:18:42 -0700

    Rendered component as variable per function call purposes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import BitByteNode from './BitByteNode.jsx'
 
 function App() {
+
+  /* Declaring as a variable for function call */
+  var node1 = <BitByteNode 
+    nodeName = "Waggie Wao"
+    treeName = "Bug - free tree"
+    since = "WI 20"
+    college = "Sixth"
+    major = "Computer Science"
+    class = "2022"
+  />
   return (
-    <BitByteNode 
-      nodeName = "Maggie"
-      treeName = "Bug - free tree"
-      college = "Sixth"
-      major = "Computer Science"
-      class = "2022"
-    />
+    node1
   );
 }
 
