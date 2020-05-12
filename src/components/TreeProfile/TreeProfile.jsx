28f62b2004217111c0c34c6f620ba2b07421c248
YiyueMaggieMao <myykitty711@gmail.com>
2020-05-12 00:10:17 -0700

    Added proptypes for tree profile

--- src/components/TreeProfile/TreeProfile.jsx
+++ src/components/TreeProfile/TreeProfile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './TreeProfile.css'
 
 function TreeProfile(props) {
@@ -6,16 +7,25 @@ function TreeProfile(props) {
         <div className="profile-wrapper">
             <div className="left">
                 <h1>{props.treeName}</h1>
+                {/* Description and points */}
                 <p>{props.treeDescription}</p>
                 <div className="points">
                     Points in {(new Date().getFullYear())}: {props.points}
                 </div>
             </div>
+            {/* Tree photo */}
             <div className="right">
                 <img className="treePhoto" src={props.treePhoto} alt="treePhoto" />
             </div>
         </div>
     )
 };
+
+TreeProfile.propTypes = {
+    treeName: PropTypes.string.isRequired,
+    treeDescription: PropTypes.string,
+    points: PropTypes.number.isRequired,
+    treePhoto: PropTypes.string,
+}
 
 export default TreeProfile;
