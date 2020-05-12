18db015cad0bf38d836ecd81459477a22d06ef0e
YiyueMaggieMao <myykitty711@gmail.com>
2020-05-12 12:51:13 -0700

    Rendered points based on active status; misc style updates

--- src/components/TreeProfile/TreeProfile.jsx
+++ src/components/TreeProfile/TreeProfile.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TreeProfile.css'
 
+/* Show points or inactive label based on active status */
+function PointSection (props){
+    if(! props.isActive){
+        return(
+            <div id="inactive">
+                Inactive
+            </div>
+        );
+    }
+    return(
+        <div>
+            {props.points} points
+        </div>
+    )
+};
+
 function TreeProfile(props) {
     return (
         <div className="profile-wrapper">
@@ -10,7 +26,11 @@ function TreeProfile(props) {
                 {/* Description and points */}
                 <p>{props.treeDescription}</p>
                 <div className="points">
-                    Points in {(new Date().getFullYear())}: {props.points}
+                    <div className="points-left">
+                        Points in {(new Date().getFullYear())}: 
+                    </div>
+                    <span>&nbsp;</span>
+                    <PointSection isActive={props.isActive} points={props.points}/>
                 </div>
             </div>
             {/* Tree photo */}
@@ -25,6 +45,7 @@ TreeProfile.propTypes = {
     treeName: PropTypes.string.isRequired,
     treeDescription: PropTypes.string,
     points: PropTypes.number.isRequired,
+    isActive: PropTypes.bool.isRequired,
     treePhoto: PropTypes.string,
 }
 
