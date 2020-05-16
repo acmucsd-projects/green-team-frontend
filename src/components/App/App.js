6038d3d9114d80b1adc3c63f479d2fd0d98a88e5
YiyueMaggieMao <myykitty711@gmail.com>
2020-05-16 15:16:50 -0700

    Leaderboard renders all active trees in order of appearance in json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,16 @@ import FamilyTree from '../FamilyTree/FamilyTree'
 import TreeProfile from '../TreeProfile/TreeProfile'
 import treeData from '../../data/tree-data.json'
 import treePhoto from '../../img/tree_profiles/bug_free_tree.jpg'
+import Leaderboard from '../Leaderboard/Leaderboard'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Leaderboard/>
+      {/*<LbRow rank = {1} tree_name="Bug-free tree" points={50}/>*/}
       {/* Tree profile section */}
-      <div className="tree-profile">
+      {/*<div className="tree-profile">
         <TreeProfile 
           treeName="Bug-free Tree" 
           treeDescription="Hello! We are a tree that aims to have no bugs in our code. We bond over lots and lots of chicken wings, and we never get tired of LeetCode!"
@@ -17,7 +20,7 @@ function App() {
           points={0}
           isActive={false}
         />
-      </div>
+  </div> */}
       {/* Actual tree */}
       <div className = "tree">
         <FamilyTree treeData={treeData}/>
---
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Leaderboard.css';
+import LbRow from '../LbRow/LbRow';
+import points from '../../data/tree-points.json';
+
+var rank = 0;
+
+/* Basically all the active trees with the first tree having the top rank */
+const lbBody = points.map( (data) => {
+        return(
+            data.active&&
+            <LbRow rank={++rank} tree_name={data.tree_name} points={data.points}/>
+        );
+    }
+);
+
+function Leaderboard(){
+    return(
+        <div className="table">
+            <div className="table-heading">
+            <div id="heading-rank">Rank</div>
+            <div id="heading-tree-name">Tree Name</div>
+            <div id="heading-points">Points</div>
+        </div>
+            {lbBody}
+        </div>
+    );
+}
+
+export default Leaderboard;
